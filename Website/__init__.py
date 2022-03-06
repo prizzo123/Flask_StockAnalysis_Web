07e908959592e231e0f9e79f69155574dc2b3cd1
prizzo123 <32901313+prizzo123@users.noreply.github.com>
2022-03-06 08:26:45 -0500

    Add files via upload

--- Website/__init__.py
+++ Website/__init__.py
@@ -1,14 +1,16 @@
 from flask import Flask
 
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'qwerty123'
-
+    
     from views import views
     from auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    return(app)
+    from models import User, Note
 
+    return(app)
